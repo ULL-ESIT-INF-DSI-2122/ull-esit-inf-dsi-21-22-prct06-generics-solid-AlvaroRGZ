cc64c5886e0af1bb8d216013a988976abebf5012
AlvaroRGZ <alu0101362953@ull.edu.es>
2022-03-21 10:51:36 +0000

    Fin modi

--- src/edit.ts
+++ src/edit.ts
@@ -1,13 +1,103 @@
 /**
-* Representa la informacion de una persona
-*/
-export class Edit {
-  constructor(private n: string){}
-
+ * Collectable methods
+ */
+export interface Collectable<T> {
   /**
-   * @return nombre de la persona
+   * @param x item to add
    */
-  getN(): string {
-    return this.n;
+  addItem(x: T): void;
+  /**
+   * @param x item to get
+   */
+  getItem(x: Number): T;
+  /**
+   * @param x item to delete
+   */
+  removeItem(x: T): void;
+  /**
+   * @return number of items
+   */
+  getNumberOfItems(): number;
+}
+
+/**
+ * Printable methods
+ */
+export interface Printable<T> {
+  /**
+   * returns the representation of a collection
+   */
+  print(): string;
+}
+
+export abstract class PrintableColleccion<X> implements Printable<X>, Collectable<X>{
+  /**
+   * 
+   * @param collection vector of items collected
+   */
+  constructor(protected collection: X[]){}
+  /**
+   * @param x item to add
+   */
+  addItem(x: X): void{
+    this.collection.push(x);
+  }
+  /**
+   * @param x item to get
+   */
+  getItem(index: number): X{
+    return this.collection[index];
+  }
+  /**
+   * @param x item to remove
+   */
+  removeItem(x: X): void{
+    this.collection = this.collection.filter((e) => e != x);
+  }
+    /**
+   * @return number of items
+   */
+  getNumberOfItems(): number{
+    return this.collection.length;
+  }
+  /**
+   * @return collection info
+   */
+  abstract print(): string;
+  /**
+   * @retur the collection vector
+   */
+  getCollection(): X[]{
+    return this.collection;
+  }
+}
+
+/**
+ * A collection of numbers
+ */
+export class NumericPrintableCollection extends PrintableColleccion<Number> {
+  constructor(x: number[] = []){
+    super(x);
+  }
+  /**
+   * @return a string containing each element separated by ','
+   */
+  print(): string{
+    return this.collection.toString();
+  }
+}
+
+/**
+ * A collection of strings
+ */
+export class StringPrintableCollection extends PrintableColleccion<string> {
+  constructor(x: string[]){
+    super(x);
+  }
+  /**
+   * @return a string containing each element separated by ','
+   */
+  print(): string{
+    return this.collection.toString();
   }
 }
--- test/edit.spec.ts
+++ test/edit.spec.ts
@@ -1,10 +1,38 @@
 import 'mocha';
 import {expect} from 'chai';
-import {Edit} from '../src/edit';
+import {Collectable, Printable, PrintableColleccion,
+        NumericPrintableCollection,
+        StringPrintableCollection} from '../src/edit';
 
-describe('Pruebas', () => {
-  const e = new Edit('x');
-  it('Nombre', () => {
-    expect(e.getN()).to.be.eql('x');
+describe('Pruebas ejercicio 6', () => {
+  describe('Pruebas NumericPrintableCollection', () => {
+    const n = new NumericPrintableCollection([1,2,3,4,5]);
+    it('default constructor:', () => {
+      const d = new NumericPrintableCollection;
+      expect(d.getCollection()).to.be.deep.equal([]);
+    });
+    it('addItem:', () => {
+      n.addItem(6)
+      expect(n.getCollection()).to.be.deep.equal([1,2,3,4,5,6]);
+    });
+    it('getItem:', () => {
+      expect(n.getItem(3)).to.be.deep.equal(4);
+    });
+    it('removeItem:', () => {
+      n.removeItem(5)
+      expect(n.getCollection()).to.be.deep.equal([1,2,3,4,6]);
+    });
+    it('getNumberOfItems:', () => {
+      expect(n.getNumberOfItems()).to.be.deep.equal(5);
+    });
+    it('print:', () => {
+      expect(n.print()).to.be.deep.equal('1,2,3,4,6');
+    });
+  });
+  describe('Pruebas StringPrintableCollection', () => {
+    const s = new StringPrintableCollection(['a', 'b', 'c', 'd']);
+    it('print:', () => {
+      expect(s.print()).to.be.deep.equal('a,b,c,d');
+    });
   });
 });
